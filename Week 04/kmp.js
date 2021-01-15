096b5f48aed5b8c34aa80dd105698c0edbb355f6
Liao Xueqing <xueqingliao@foxmail.com>
2021-01-15 21:21:59 +0800

    完善kmp

--- Week 04/kmp.js
+++ Week 04/kmp.js
@@ -1,51 +1,73 @@
-function calculateTable(pattern, table) {
-  let i = 1, j = 0;
-  while (i < pattern.length) {
-    if (pattern[i] === pattern[j]) {
-      ++j;
-      ++i;
-      table[i] = j;
+function calculateTable(pattern) {
+  let table = new Array(pattern.length).fill(0);
+  let biggerIndex = 1, index = 0;
+  while (biggerIndex < pattern.length) {
+    // 匹配上了，有自重复
+    if (pattern[biggerIndex] === pattern[index]) {
+      ++index, ++biggerIndex;
+      table[biggerIndex] = index;
     } else {
-      if (j > 0) {
-        j = table[j];
+      // 没匹配上且index大于0，从头开始匹配
+      if (index > 0) {
+        index = table[index];
       } else {
-        ++i;
+        ++biggerIndex;
       }
     }
   }
+  return table;
 }
 
-function match(source, pattern, table) {
-  let i = 0, j = 0;
-  while (i < source.length) {
-    if (j === pattern.length) {
+function matchPattern(source, pattern, table) {
+  let sIndex = 0, pIndex = 0;
+  while (sIndex < source.length) {
+    // pattern最后一个单词已匹配
+    if (pIndex === pattern.length) {
       return true;
     }
-    if (pattern[j] === source[i]) {
-      ++i, ++j;
+    // 匹配上了
+    if (pattern[pIndex] === source[sIndex]) {
+      ++sIndex, ++pIndex;
     } else {
-      if (j > 0) {
-        j = table[i];
+      if (pIndex > 0) {
+        pIndex = table[pIndex];
       } else {
-        ++i;
+        ++sIndex;
       }
     }
-    return false;
   }
+  return false;
 }
 function kmp(source, pattern) {
-  console.log('----------------------')
-  console.log(source, pattern)
-  let table = new Array(pattern.length).fill(0);
-  calculateTable(pattern, table);
-  console.log('',table);
-  const result = match(source, pattern, table);
-  console.log(result);
-  console.log('----------------------')
+  const table = calculateTable(pattern);
+  const result = matchPattern(source, pattern, table);
+  return {table, result};
 }
 
+function consoleTest() {
+  const testArray = [
+    {
+      source: 'ababcdabced',
+      pattern: 'abcdabce',
+    }, {
+      source: 'abaaababababcc',
+      pattern: 'abababc',
+    }, {
+      source: 'abaaababababcc',
+      pattern: 'aabaaac',
+    }
+  ];
 
-kmp('', 'abcdabce');
-console.log(kmp('Hello', 'll'));
-console.log(kmp('abcdabcd', 'abcdabce'));
-console.log(kmp('aabaabc', 'aabaabaace'));
+  testArray.forEach(item => {
+    const {table, result} = kmp(item.source, item.pattern);
+    console.log('pattern-table-', item.pattern, table);
+    console.log('source-result-', item.source, result);
+  });
+}
+
+function startMatch() {
+  const pattern = document.getElementById('pattern').value;
+  const source = document.getElementById('source').value;
+  const {table, result} = kmp(source, pattern);
+  document.getElementById('result').innerText = `table:${table}。 result:${result}`;
+}
