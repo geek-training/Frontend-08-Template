6a898906c11ac54714a0aae3d31dcafa321e833b
Liao Xueqing <xueqingliao@foxmail.com>
2021-01-16 18:22:00 +0800

    修复trie

--- Week 04/trie.js
+++ Week 04/trie.js
@@ -1,4 +1,4 @@
-const endChar = Symbol('$');
+const endChar = Symbol("$");
 class Trie {
   constructor() {
     this.root = Object.create(null);
@@ -12,35 +12,38 @@ class Trie {
       }
       node = node[c];
     }
-    if(!(endChar in node)) {
-      node.endChar = 0;
+    if (!(endChar in node)) {
+      node[endChar] = 0;
     }
-    node.endChar ++;
+    node[endChar]++;
   }
 
   most() {
     let max = 0;
     let maxWord = null;
-    let visit = (node, word) =>  {
-      if (node.endChar && node.endChar > max) {
-        max = node.endChar;
+    let visit = (node, word) => {
+      if (node[endChar] && node[endChar] > max) {
+        max = node[endChar];
         maxWord = word;
       }
-    }
-    visit(this.root, '');
+      for (let p in node) {
+        visit(node[p], word + p);
+      }
+    };
+    visit(this.root, "");
     console.log(maxWord, max);
   }
 }
 
 function randomWord(length) {
-  let randomStr = '';
+  let randomStr = "";
   for (let i = 0; i < length; i++) {
-    randomStr += String.fromCharCode(Math.random() * 26 + 'a'.charCodeAt(0));
+    randomStr += String.fromCharCode(Math.random() * 26 + "a".charCodeAt(0));
   }
   return randomStr;
 }
 
 let trie = new Trie();
-for (let i = 0; i < 100; i++) {
+for (let i = 0; i < 10000; i++) {
   trie.insert(randomWord(4));
 }
