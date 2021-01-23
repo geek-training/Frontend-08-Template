c0c863860150215606ac0670e5a1319ff67cb8f3
xueqing <1047504737@qq.com>
2021-01-23 17:44:28 +0800

    拖拽改变布局

--- Week 05/NOTE.md
+++ Week 05/NOTE.md
@@ -1,1 +1,10 @@
 学习笔记
+
+### 不选中文字效果
+1. user-select:none
+2. document.addEventListener('selectstart', event => event.preventDefault());
+
+### CSSDOM
+
+
+
--- Week 05/dragable.js
+++ Week 05/dragable.js
@@ -12,8 +12,36 @@ dragable.addEventListener('mousedown', function (event) {
         document.removeEventListener('mouseup', up);
     };
     let move = event => {
-        dragable.style.transform = `translate(${baseX + event.clientX - startX}px, ${baseY + event.clientY - startY}px)`;
+        // dragable.style.transform = `translate(${baseX + event.clientX - startX}px, ${baseY + event.clientY - startY}px)`;
+        let range = getNearest(event.clientX, event.clientY);
+        range.insertNode(dragable);
     };
     document.addEventListener('mousemove', move);
     document.addEventListener('mouseup', up);
 })
+
+let ranges = [];
+
+let container = document.getElementById('container');
+for (let i = 0; i < container.childNodes[0].textContent.length; i++) {
+    let range = document.createRange();
+    range.setStart(container.childNodes[0], i);
+    range.setEnd(container.childNodes[0], i);
+
+    console.log(range.getBoundingClientRect());
+    ranges.push(range);
+}
+
+function getNearest(x, y) {
+    let min = Infinity;
+    let nearest = null;
+    for(let range of ranges) {
+        let rect = range.getBoundingClientRect();
+        let distance = (rect.x - x) ** 2 + (rect.y - y) ** 2;
+        if (distance < min) {
+            nearest = range;
+            min = distance;
+        }
+    }
+    return nearest;
+}
