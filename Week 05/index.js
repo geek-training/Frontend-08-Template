744847e9c9268e5d9479278959570457c9491f51
xueqing <1047504737@qq.com>
2021-01-23 16:05:10 +0800

    增加usedReactivities

--- Week 05/index.js
+++ Week 05/index.js
@@ -1,4 +1,6 @@
-let callbacks = [];
+let callbacks = new Map();
+
+let usedReactivities = [];
 let object = {
     a: 1,
     b: 2,
@@ -11,22 +13,41 @@ effect(() => {
 })
 
 function effect(callback) {
-    callbacks.push(callback);
+    // callbacks.push(callback);
+    usedReactivities = [];
+    callback();
+    console.log(usedReactivities);
+    for (let reactivity of usedReactivities) {
+        if (!callbacks.has(reactivity[0])) {
+            callbacks.set(reactivity[0], new Map());
+        }
+        if (!callbacks.get(reactivity[0]).has(reactivity[1])) {
+            callbacks.get(reactivity[0]).set(reactivity[1], []);
+        }
+        callbacks.get(reactivity[0]).get(reactivity[1]).push(callback);
+    }
 }
 
 function reactive(obj) {
     return new Proxy(obj, {
         set(obj, prop, val) {
             obj[prop] = val;
-            for (let callback of callbacks) {
-                callback();
+            if (callbacks.get(obj)) {
+                if (callbacks.get(obj).get(prop)) {
+                    for (let callback of callbacks.get(obj).get(prop)) {
+                        callback();
+                    }
+                }
             }
             return obj[prop];
         },
 
         get(obj, prop) {
-            console.log(obj, prop);
+            usedReactivities.push([obj, prop]);
             return obj[prop];
         }
     })
 }
+
+console.log(po.a);
+console.log(po.b);
