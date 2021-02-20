a13cb9dda23ff1ef3bd4177c825634cc1763bce4
Liao Xueqing <xueqingliao@foxmail.com>
2021-02-20 21:29:37 +0800

    状态机处理完全未知的pattern

--- Week 08/state-machine/kmp.js
+++ Week 08/state-machine/kmp.js
@@ -2,77 +2,65 @@
  * 状态机处理完全未知的pattern
  */
 function createKMPMatcher(pattern) {
-    let states = initState(pattern);
+  let end = () => end;
+  let start = (ch) => {
+      console.log('Current char:', ch, 'Current Index: 0');
+      return ch == pattern[0] ? states[1] : states[0]
+  }
 
-    let table = Array(pattern.length).fill(0);
-    let biggerIndex = 1, index = 0
-    while (biggerIndex < pattern.length) {
-        // 匹配上了，有自重复
-        if (pattern[biggerIndex] == pattern[index]) { 
-
-            ++biggerIndex, ++index;
-
-            table[biggerIndex] = index
-            states[biggerIndex] = function (ch) {
-                console.log('Char      ：', ch)
-                console.log('State     ：', biggerIndex, 'Expect：', pattern[biggerIndex])
-                console.log('Next State：', biggerIndex + 1, ' or ', index)
-                return ch == pattern[biggerIndex] ? states[biggerIndex + 1] : states[index](ch)
-            }
-        } else {
-            // 没匹配上且 index > 0，从头开始匹配
-            if (index > 0) {
-                index = table[index]
-            } else {
-                ++biggerIndex
-            }
+  let states = [start];
+  for (let i = 1; i < pattern.length; ++i) {
+        let state = (ch) => {
+            console.log('Current char:', ch, 'Current Index:', i);
+            return ch == pattern[i] ? states[i+1] : states[0](ch)
         }
-    }
-    console.log('table:', table);
-    console.log('states:', states);
+        states.push(state);
+  }
+  states.push(end);
 
-    return states;
+  let table = Array(pattern.length).fill(0);
+  let biggerIndex = 1;
+  let index = 0;
+  while (biggerIndex < pattern.length) {
+    // 匹配上了，有自重复
+    if (pattern[biggerIndex] == pattern[index]) {
+      const currentBiggerIndex = ++biggerIndex;
+      const currentIndex = ++index;
+      if (table[biggerIndex] != index) {
+        // 更新table
+        table[biggerIndex] = index;
+        // 更新states
+        let currentState = (ch) => {
+          console.log('- Current char:', ch, 'Current Index', currentBiggerIndex);
+          return ch == pattern[currentBiggerIndex] ? states[currentBiggerIndex + 1] : states[currentIndex](ch);
+        }
+        states[biggerIndex] = currentState;
+      }
+    } else {
+      // 没匹配上且 index > 0，从头开始匹配
+      if (index > 0) {
+        index = table[index];
+      } else {
+        ++biggerIndex;
+      }
+    }
+  }
+  table = null;
+  return [start, end];
 }
 
-function initState(pattern) {
-    let end = () => end
-    let states = [
-        ch => {
-            console.log('Char      ：', ch)
-            console.log('State     ：', 0, 'Expect：', pattern[0])
-            console.log('Next State：', 1, ' or ', 0)
-            return ch == pattern[0] ? states[1] : states[0]
-        }
-    ]
-
-    for (let i = 1; i < pattern.length; ++i) {
-        states.push(
-            ch => {
-                console.log('Char      ：', ch)
-                console.log('State     ：', i, 'Expect：', pattern[i])
-                console.log('Next State：', i + 1, ' or ', 0)
-                return ch == pattern[i] ? states[i + 1] : states[0](ch)
-            }
-        )
-    }
-
-    states.push(end)
-    return states;
-}
-
-function match(source, states, end) {
-    let state = init
-    for (const ch of source) {
-        state = state(ch)
-    }
-    return state === end
+function match(source, init, end) {
+  let state = init;
+  for (const ch of source) {
+    state = state(ch);
+  }
+  return state === end;
 }
 
 function KMP(pattern, source) {
-    let matcher = createKMPMatcher(pattern)
-
-    return match(source, matcher, matcher[matcher.length - 1])
+  let [start, end] = createKMPMatcher(pattern);
+  return match(source, start, end);
 }
 
 // expect table [0, 0, 0, 1, 2, 3, 4]
-console.log(KMP('abababx', 'xxxabababxxx'));
+console.log(KMP("abababx", "abababxxx"));
