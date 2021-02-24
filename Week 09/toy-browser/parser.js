7bc5b7103669929b6d5c159d4099a099abafe46c
xueqing <1047504737@qq.com>
2021-02-24 12:27:11 +0800

    解析标签

--- Week 09/toy-browser/parser.js
+++ Week 09/toy-browser/parser.js
@@ -1,3 +1,105 @@
+const EOF = Symbol('EOF');
+
+let currentToken = null;
+function emit(token) {
+    console.log(token);
+}
+
+function data(c) {
+    if (c == '<') {
+        return tagOpen;
+    } else if (c == EOF) {
+        emit({
+            type: 'EOF'
+        });
+        return ;
+    } else {
+        // 文本节点
+        return data;
+    }
+}
+
+/**
+ * 标签开始
+ * @param c
+ * @returns {endTagOpen|*}
+ */
+function tagOpen(c) {
+    if (c == '/') {
+        return endTagOpen;
+    } else if (c.match(/^[a-zA-Z]$/)) {
+        return tagName(c);
+    } else {
+
+    }
+
+}
+
+/**
+ * 结束标签的开始
+ * @param c
+ */
+function endTagOpen(c) {
+    if(c.match(/^[a-zA-Z]$/)) {
+        return tagName(c);
+    } else if (c == '>') {
+        // 报错
+    } else if (c == EOF) {
+        // 报错
+    } else {
+
+    }
+}
+
+function tagName(c) {
+    /**
+     * tagName以空白符结束
+     * html中有效的4种空白符
+     */
+    if(c.match(/^[\t\n\f ]$/)) {
+        return beforeAttributeName;
+    } else if(c == '/') {
+        return selfClosingStartTag;
+    } else if(c.match(/^[a-zA-Z]$/)) {
+        return tagName;
+    } else if (c == '>') {
+        /**
+         * 结束标签回到data状态解析下一个标签
+         */
+        return data;
+    } else {
+        return tagName;
+    }
+}
+
+function beforeAttributeName(c) {
+    if(c.match(/^[\t\n\f ]$/)) {
+        return beforeAttributeName;
+    } else if (c == '>') {
+        return data;
+    } else if (c == '=') {
+        return beforeAttributeName;
+    } else {
+        return beforeAttributeName;
+    }
+}
+
+function selfClosingStartTag(c) {
+    if(c == '>') {
+        currentToken.isSelfClosing = true;
+        return data;
+    } else if (c == EOF) {
+
+    } else {
+
+    }
+}
+
 module.exports.parseHTML = function parseHTML(html) {
     console.log(html);
+    let state = data;
+    for (let c of html) {
+        state = state(c);
+    }
+    state = state(EOF);
 }
