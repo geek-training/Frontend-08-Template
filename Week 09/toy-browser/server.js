402cecd2fbf8721cad8d67549c97e488d5ed40ef
xueqing <1047504737@qq.com>
2021-02-24 11:11:00 +0800

    Init Week 09 and add parser.js File

---
+++ Week 09/toy-browser/server.js
@@ -0,0 +1,36 @@
+const http = require('http');
+
+const server = http.createServer((request, response) => {
+    console.log('request received!');
+    console.log(request.headers);
+    response.setHeader('Content-Type', 'text/html');
+    response.setHeader('X-Foo', 'bar');
+    response.writeHead(200, {'Content-Type': 'text/plain'});
+    response.end(
+`
+<html maa=a>
+    <head>
+        <style>
+        body div #myId {
+            width: 100px;
+            background-color: #00ff00;
+        }
+        body div img {
+        width: 30px;
+        background-color: #f11;
+        }
+        </style>
+    </head>
+    <body>
+        <div>
+            <img id="myId" />
+            <img />
+        </div>
+    </body>
+</html>
+`
+    );
+});
+
+server.listen(8088);
+console.log('server started');
