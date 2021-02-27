c22a3340d59c4e0b576425178572356ed3aa13a3
Liao Xueqing <xueqingliao@foxmail.com>
2021-02-27 16:31:48 +0800

    生成computed属性

--- Week 09/toy-browser/client.js
+++ Week 09/toy-browser/client.js
@@ -1,23 +1,22 @@
-const Request = require('./request');
-const parser = require('./parser');
+const Request = require("./request");
+const parser = require("./parser");
 
-void async function() {
-    let request = new Request({
-        method: 'POST',
-        host: '127.0.0.1',
-        port: '8088',
-        path: '/',
-        headers: {
-            ['X-Foo2']: 'customer'
-        },
-        body: {
-            name: 'xq'
-        }
-    });
+void (async function () {
+  let request = new Request({
+    method: "POST",
+    host: "127.0.0.1",
+    port: "8088",
+    path: "/",
+    headers: {
+      ["X-Foo2"]: "customer",
+    },
+    body: {
+      name: "xq",
+    },
+  });
 
-    let response = await request.send();
+  let response = await request.send();
 
-    let dom = parser.parseHTML(response.body);
-    console.log(dom);
-    console.log('--end---');
-}();
+  let dom = parser.parseHTML(response.body);
+  console.log("dom", dom);
+})();
--- Week 09/toy-browser/parser.js
+++ Week 09/toy-browser/parser.js
@@ -1,4 +1,4 @@
-const css = require('css');
+const css = require("css");
 
 /**
  * tagName以空白符结束
@@ -10,167 +10,174 @@ const REG_BLANK_CHARACTER = /^[\t\n\f ]$/;
  */
 const REG_LETTER = /^[a-zA-Z]$/;
 
-const EOF = Symbol('EOF');
+const EOF = Symbol("EOF");
 
 let currentToken = null;
 let currentAttribute = null;
-let stack = [{type: 'document', children: []}];
+let stack = [{ type: "document", children: [] }];
 let currentTextNode = null;
-
 
 let rules = [];
 function addCssRules(text) {
-    let ast = css.parse(text);
-    rules.push(...ast.stylesheet.rules);
+  let ast = css.parse(text);
+  rules.push(...ast.stylesheet.rules);
 }
 
 /**
- * 只考虑简单选择器 
+ * 只考虑简单选择器
  * div
  * .name
  * #title
  * 选做：支持 class='img myImg'
  */
 function match(element, selector) {
-    if (!selector || !element.attributes) {
-        return false;
-    }
-
-    const startChar = selector.charAt(0);
-    if (startChar == '#') {
-        const attr = element.attributes.filter(attr => attr.name === 'id');
-        if (attr.length !== 0 && attr[0].value === selector.replace('#', '')) {
-            return true;
-        }
-    } else if (startChar == '.') {
-        const classAttr = element.attributes.filter(attr => attr.name === 'class');
-        if (!classAttr || classAttr.length === 0) {
-            return false;
-        }
-
-        const classArray = classAttr[0].value.split(' ');
-        for (let i = 0; i < classArray.length; i++) {
-            if (classArray[i] === selector.replace('.', '')) {
-                return true;
-            }
-        }
-    } else {
-        if (element.tagName === selector) {
-            return true;
-        }
-    }
+  if (!selector || !element.attributes) {
     return false;
+  }
+
+  const startChar = selector.charAt(0);
+  if (startChar == "#") {
+    const attr = element.attributes.filter((attr) => attr.name === "id");
+    if (attr.length !== 0 && attr[0].value === selector.replace("#", "")) {
+      return true;
+    }
+  } else if (startChar == ".") {
+    const classAttr = element.attributes.filter(
+      (attr) => attr.name === "class"
+    );
+    if (!classAttr || classAttr.length === 0) {
+      return false;
+    }
+
+    const classArray = classAttr[0].value.split(" ");
+    for (let i = 0; i < classArray.length; i++) {
+      if (classArray[i] === selector.replace(".", "")) {
+        return true;
+      }
+    }
+  } else {
+    if (element.tagName === selector) {
+      return true;
+    }
+  }
+  return false;
 }
 
 function computeCSS(element) {
-    let parentElements = stack.slice().reverse();
-    if (!element.computedStyle) {
-        element.computedStyle = {};
+  let parentElements = stack.slice().reverse();
+  if (!element.computedStyle) {
+    element.computedStyle = {};
+  }
+
+  for (let rule of rules) {
+    /**
+     * body div #myId
+     * ['#myId', 'div', 'body']
+     * 如果第一个 #myId 没匹配上就跳过本次循环
+     */
+    let selectorParts = rule.selectors[0].split(" ").reverse();
+
+    if (!match(element, selectorParts[0])) {
+      continue;
     }
 
-    for (let rule of rules) {
-        /**
-         * body div #myId
-         * ['#myId', 'div', 'body']
-         * 如果第一个 #myId 没匹配上就跳过本次循环
-         */
-        let selectorParts = rule.selectors[0].split(' ').reverse();
-
-        if (!match(element, selectorParts[0])) {
-            continue;
-        }
-
-        /**
-         * #myId 已经匹配上来，接下来从 selectorParts[1] 开始去逐层匹配当前元素的父元素
-         */
-        let j = 1;
-        for (let i = 0; i < parentElements.length; i++) {
-            if (match(parentElements[i], selectorParts[j])) {
-                j++;
-            }
-        }
-        /** 【‘#myId', 'div', 'body'】每项都匹配到了，则为true */
-        if (j >= selectorParts.length) {
-            matched = true;
-        }
-        if (matched) {
-            /** 如果匹配到，我们要加入 */
-            console.log('Matched ------ Element', element, 'matched rule', rule);
-        }
+    /**
+     * #myId 已经匹配上来，接下来从 selectorParts[1] 开始去逐层匹配当前元素的父元素
+     */
+    let j = 1;
+    for (let i = 0; i < parentElements.length; i++) {
+      if (match(parentElements[i], selectorParts[j])) {
+        j++;
+      }
     }
+    /** 【‘#myId', 'div', 'body'】每项都匹配到了，则为true */
+    if (j >= selectorParts.length) {
+      matched = true;
+    }
+    if (matched) {
+      /** 如果匹配到，我们要加入 */
+      const computedStyle = element.computedStyle;
+      for (const declaration of rule.declarations) {
+        if (!computedStyle[declaration.property]) {
+          computedStyle[declaration.property] = {};
+        }
+        computedStyle[declaration.property].value = declaration.value;
+      }
+      console.log("+++ Matched +++", element.computedStyle);
+    }
+  }
 }
 
 function emit(token) {
-    let top = stack[stack.length - 1];
+  let top = stack[stack.length - 1];
 
-    if (token.type == 'startTag') {
-        let element = {
-            type: 'element',
-            children: [],
-            attributes: []
-        };
+  if (token.type == "startTag") {
+    let element = {
+      type: "element",
+      children: [],
+      attributes: [],
+    };
 
-        element.tagName = token.tagName;
+    element.tagName = token.tagName;
 
-        for (let p in token) {
-            if (p != 'type' && p != 'tagName') {
-                element.attributes.push({
-                    name: p,
-                    value: token[p]
-                });
-            }
-        }
+    for (let p in token) {
+      if (p != "type" && p != "tagName") {
+        element.attributes.push({
+          name: p,
+          value: token[p],
+        });
+      }
+    }
 
-        computeCSS(element);
+    computeCSS(element);
 
-        top.children.push(element);
-        element.parent = top;
+    top.children.push(element);
+    element.parent = top;
 
-        if (!token.isSelfClosing) {
-            stack.push(element);
-        }
+    if (!token.isSelfClosing) {
+      stack.push(element);
+    }
 
-        currentTextNode = null;
-    } else if (token.type == 'endTag') {
-        if (top.tagName != token.tagName) {
-            throw new Error(`Tag start end doesn't match!`);
-        } else {
-            if (top.tagName === 'style') {
-                /** 遇到style标签时，执行添加 css 规则的操作  */
-                addCssRules (top.children[0].content);
-            }
-            stack.pop();
-        }
-        currentTextNode = null;
-    } else if (token.type == 'text') {
-        if (currentTextNode == null) {
-            currentTextNode = {
-                type: 'text',
-                content: ''
-            };
-            top.children.push(currentTextNode);
-        }
-        currentTextNode.content += token.content;
-    } 
-    
+    currentTextNode = null;
+  } else if (token.type == "endTag") {
+    if (top.tagName != token.tagName) {
+      throw new Error(`Tag start end doesn't match!`);
+    } else {
+      if (top.tagName === "style") {
+        /** 遇到style标签时，执行添加 css 规则的操作  */
+        addCssRules(top.children[0].content);
+      }
+      stack.pop();
+    }
+    currentTextNode = null;
+  } else if (token.type == "text") {
+    if (currentTextNode == null) {
+      currentTextNode = {
+        type: "text",
+        content: "",
+      };
+      top.children.push(currentTextNode);
+    }
+    currentTextNode.content += token.content;
+  }
 }
 
 function data(c) {
-    if (c == '<') {
-        return tagOpen;
-    } else if (c == EOF) {
-        emit({
-            type: 'EOF'
-        });
-        return ;
-    } else {
-        // 文本节点
-        emit({
-           type: 'text',
-           content: c,
-        });
-        return data;
-    }
+  if (c == "<") {
+    return tagOpen;
+  } else if (c == EOF) {
+    emit({
+      type: "EOF",
+    });
+    return;
+  } else {
+    // 文本节点
+    emit({
+      type: "text",
+      content: c,
+    });
+    return data;
+  }
 }
 
 /**
@@ -178,18 +185,17 @@ function data(c) {
  * 前一状态：data
  */
 function tagOpen(c) {
-    if (c == '/') {
-        return endTagOpen;
-    } else if (c.match(REG_LETTER)) {
-        currentToken = {
-            type: 'startTag',
-            tagName: '',
-        }
-        return tagName(c);
-    } else {
-        return ;
-    }
-
+  if (c == "/") {
+    return endTagOpen;
+  } else if (c.match(REG_LETTER)) {
+    currentToken = {
+      type: "startTag",
+      tagName: "",
+    };
+    return tagName(c);
+  } else {
+    return;
+  }
 }
 
 /**
@@ -198,19 +204,18 @@ function tagOpen(c) {
  * 前一状态：tagOpen
  */
 function endTagOpen(c) {
-    if(c.match(REG_LETTER)) {
-        currentToken = {
-            type: 'endTag',
-            tagName: '',
-        }
-        return tagName(c);
-    } else if (c == '>') {
-        // 报错
-    } else if (c == EOF) {
-        // 报错
-    } else {
-
-    }
+  if (c.match(REG_LETTER)) {
+    currentToken = {
+      type: "endTag",
+      tagName: "",
+    };
+    return tagName(c);
+  } else if (c == ">") {
+    // 报错
+  } else if (c == EOF) {
+    // 报错
+  } else {
+  }
 }
 
 /**
@@ -220,19 +225,19 @@ function endTagOpen(c) {
  * 前一状态：tagOpen，endTagOpen
  */
 function tagName(c) {
-    if(c.match(REG_BLANK_CHARACTER)) {
-        return beforeAttributeName;
-    } else if(c == '/') {
-        return selfClosingStartTag;
-    } else if(c.match(REG_LETTER)) {
-        currentToken.tagName += c;
-        return tagName;
-    } else if (c == '>') {
-        emit(currentToken);
-        return data;
-    } else {
-        return tagName;
-    }
+  if (c.match(REG_BLANK_CHARACTER)) {
+    return beforeAttributeName;
+  } else if (c == "/") {
+    return selfClosingStartTag;
+  } else if (c.match(REG_LETTER)) {
+    currentToken.tagName += c;
+    return tagName;
+  } else if (c == ">") {
+    emit(currentToken);
+    return data;
+  } else {
+    return tagName;
+  }
 }
 
 /**
@@ -241,15 +246,13 @@ function tagName(c) {
  * 前一状态：tagName, unquotedAttributeValue, afterQuotedAttributeValue, afterAttributeName
  */
 function selfClosingStartTag(c) {
-    if(c == '>') {
-        currentToken.isSelfClosing = true;
-        emit(currentToken);
-        return data;
-    } else if (c == EOF) {
-
-    } else {
-
-    }
+  if (c == ">") {
+    currentToken.isSelfClosing = true;
+    emit(currentToken);
+    return data;
+  } else if (c == EOF) {
+  } else {
+  }
 }
 
 /**
@@ -258,19 +261,19 @@ function selfClosingStartTag(c) {
  * 前一状态 tagName, unquotedAttributeValue, afterQuotedAttributeValue
  */
 function beforeAttributeName(c) {
-    if(c.match(REG_BLANK_CHARACTER)) {
-        return beforeAttributeName;
-    } else if (c == '/' || c == '>' || c == EOF) {
-        return afterAttributeName(c);
-    } else if (c == '=') {
-        // return beforeAttributeName;
-    } else {
-        currentAttribute = {
-            name: '',
-            value: '',
-        };
-        return attributeName(c);
-    }
+  if (c.match(REG_BLANK_CHARACTER)) {
+    return beforeAttributeName;
+  } else if (c == "/" || c == ">" || c == EOF) {
+    return afterAttributeName(c);
+  } else if (c == "=") {
+    // return beforeAttributeName;
+  } else {
+    currentAttribute = {
+      name: "",
+      value: "",
+    };
+    return attributeName(c);
+  }
 }
 
 /**
@@ -278,134 +281,122 @@ function beforeAttributeName(c) {
  * eg: <div title='hello div'> t 开始进入该方法
  */
 function attributeName(c) {
-    if (c.match(REG_BLANK_CHARACTER) || c == '/' || c == EOF) {
-        return afterAttributeName(c);
-    } else if (c == '=') {
-        return beforeAttributeValue;
-    } else if (c == '\u0000') {
-
-    } else if (c == '"' || c == '\'' || c == '<') {
-    } else {
-        currentAttribute.name += c;
-        return attributeName;
-    }
-
+  if (c.match(REG_BLANK_CHARACTER) || c == "/" || c == EOF) {
+    return afterAttributeName(c);
+  } else if (c == "=") {
+    return beforeAttributeValue;
+  } else if (c == "\u0000") {
+  } else if (c == '"' || c == "'" || c == "<") {
+  } else {
+    currentAttribute.name += c;
+    return attributeName;
+  }
 }
 
-function beforeAttributeValue (c) {
-    if (c.match(REG_BLANK_CHARACTER) || c == '/' || c == '>' || c == EOF) {
-        return beforeAttributeValue;
-    } else if (c == '"') {
-        return doubleQuotedAttributeValue;
-    } else if (c == '\'') {
-        return singleQuotedAttributeValue;
-    } else if (c == '>') {
-
-    } else {
-        return unquotedAttributeValue(c);
-    }
+function beforeAttributeValue(c) {
+  if (c.match(REG_BLANK_CHARACTER) || c == "/" || c == ">" || c == EOF) {
+    return beforeAttributeValue;
+  } else if (c == '"') {
+    return doubleQuotedAttributeValue;
+  } else if (c == "'") {
+    return singleQuotedAttributeValue;
+  } else if (c == ">") {
+  } else {
+    return unquotedAttributeValue(c);
+  }
 }
 
 function doubleQuotedAttributeValue(c) {
-    if (c == '"') {
-        currentToken[currentAttribute.name] = currentAttribute.value;
-        return afterQuotedAttributeValue;
-    } else if (c == '\u0000') {
-
-    } else if (c == EOF) {
-
-    } else {
-        currentAttribute.value += c;
-        return doubleQuotedAttributeValue;
-    }
+  if (c == '"') {
+    currentToken[currentAttribute.name] = currentAttribute.value;
+    return afterQuotedAttributeValue;
+  } else if (c == "\u0000") {
+  } else if (c == EOF) {
+  } else {
+    currentAttribute.value += c;
+    return doubleQuotedAttributeValue;
+  }
 }
 
 function singleQuotedAttributeValue(c) {
-    if (c == '\'') {
-        currentToken[currentAttribute.name] = currentAttribute.value;
-        return afterQuotedAttributeValue;
-    } else if (c == '\u0000') {
-
-    } else if (c == EOF) {
-
-    } else {
-        currentAttribute.value += c;
-        return singleQuotedAttributeValue;
-    }
+  if (c == "'") {
+    currentToken[currentAttribute.name] = currentAttribute.value;
+    return afterQuotedAttributeValue;
+  } else if (c == "\u0000") {
+  } else if (c == EOF) {
+  } else {
+    currentAttribute.value += c;
+    return singleQuotedAttributeValue;
+  }
 }
 
 function unquotedAttributeValue(c) {
-    if (c.match(REG_BLANK_CHARACTER)) {
-        currentToken[currentAttribute.name] = currentAttribute.value;
-        return beforeAttributeName;
-    } else if (c == '/') {
-        currentToken[currentAttribute.name] = currentAttribute.value;
-        return selfClosingStartTag;
-    } else if (c == '>') {
-        currentToken[currentAttribute.name] = currentAttribute.value;
-        emit(currentToken);
-        return data;
-    } else if (c == '\u0000') {
-
-    } else if (c == '"' || c == '\'' || c == '<' || c == '=' || c == '`') {
-
-    } else if (c == EOF) {
-
-    } else {
-        currentAttribute.value += c;
-        return unquotedAttributeValue;
-    }
+  if (c.match(REG_BLANK_CHARACTER)) {
+    currentToken[currentAttribute.name] = currentAttribute.value;
+    return beforeAttributeName;
+  } else if (c == "/") {
+    currentToken[currentAttribute.name] = currentAttribute.value;
+    return selfClosingStartTag;
+  } else if (c == ">") {
+    currentToken[currentAttribute.name] = currentAttribute.value;
+    emit(currentToken);
+    return data;
+  } else if (c == "\u0000") {
+  } else if (c == '"' || c == "'" || c == "<" || c == "=" || c == "`") {
+  } else if (c == EOF) {
+  } else {
+    currentAttribute.value += c;
+    return unquotedAttributeValue;
+  }
 }
 
 function afterQuotedAttributeValue(c) {
-    if (c.match(REG_BLANK_CHARACTER)) {
-        return beforeAttributeName;
-    } else if (c == '/') {
-        return selfClosingStartTag;
-    } else if (c == '>') {
-        currentToken[currentAttribute.name] = currentAttribute.value;
-        emit(currentToken);
-        return data;
-    } else if (c == EOF) {
-
-    } else {
-        currentAttribute.value += c;
-        return doubleQuotedAttributeValue;
-    }
+  if (c.match(REG_BLANK_CHARACTER)) {
+    return beforeAttributeName;
+  } else if (c == "/") {
+    return selfClosingStartTag;
+  } else if (c == ">") {
+    currentToken[currentAttribute.name] = currentAttribute.value;
+    emit(currentToken);
+    return data;
+  } else if (c == EOF) {
+  } else {
+    currentAttribute.value += c;
+    return doubleQuotedAttributeValue;
+  }
 }
 
 /**
  * attributeName 后面跟了空格，进入该方法
  */
 function afterAttributeName(c) {
-    if (c.match(REG_BLANK_CHARACTER)) {
-        return afterAttributeName;
-    } else if (c == '/') {
-        return selfClosingStartTag;
-    } else if (c == '=') {
-        return beforeAttributeValue;
-    } else if (c == '>') {
-        currentToken[currentAttribute.name] = currentAttribute.value;
-        emit(currentToken);
-        return data;
-    } else if (c == EOF) {
-
-    } else {
-        currentToken[currentAttribute.name] = currentAttribute.value;
-        currentAttribute = {
-            name: '',
-            value: '',
-        };
-        return attributeName(c);
-    }
+  if (c.match(REG_BLANK_CHARACTER)) {
+    return afterAttributeName;
+  } else if (c == "/") {
+    return selfClosingStartTag;
+  } else if (c == "=") {
+    return beforeAttributeValue;
+  } else if (c == ">") {
+    currentToken[currentAttribute.name] = currentAttribute.value;
+    emit(currentToken);
+    return data;
+  } else if (c == EOF) {
+  } else {
+    currentToken[currentAttribute.name] = currentAttribute.value;
+    currentAttribute = {
+      name: "",
+      value: "",
+    };
+    return attributeName(c);
+  }
 }
 
 module.exports.parseHTML = function parseHTML(html) {
-    console.log(html);
-    let state = data;
-    for (let c of html) {
-        state = state(c);
-    }
-    state = state(EOF);
-    return stack;
-}
+  console.log(html);
+  let state = data;
+  for (let c of html) {
+    state = state(c);
+  }
+  state = state(EOF);
+  return stack;
+};
