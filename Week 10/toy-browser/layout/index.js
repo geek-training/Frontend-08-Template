9a84b5b590363ebd8a6fa3d828c8b4e79b49e8de
xueqing <1047504737@qq.com>
2021-03-01 18:01:08 +0800

    分行

--- Week 10/toy-browser/layout/index.js
+++ Week 10/toy-browser/layout/index.js
@@ -122,6 +122,73 @@ function layout(element) {
         crossBase = 0;
         crossSign = 1;
     }
+
+    let isAutoMainSize = false;
+    if (!style[mainSize]) {
+        elementStyle[mainSize] = 0;
+        for(let i = 0; i < items.length; i++) {
+            const item = items[i];
+            if (itemStyle[mainSize] !== null || itemStyle[mainSize]) {
+                elementStyle[mainSize] = elementStyle[mainSize]
+            }
+        }
+        isAutoMainSize = true;
+    }
+
+    let flexLine = [];
+    let flexLines = [flexLine];
+
+    // 剩余空间
+    let mainSpace = elementStyle[mainSize];
+    let crossSpace = 0;
+    for(let i = 0; i < items.length; i++) {
+        const item = items[i];
+        let itemStyle = getStyle(item);
+
+        if (itemStyle[mainSize] === null) {
+            itemStyle[mainSize] = 0;
+        }
+
+        if (itemStyle.flex) {
+            flexLine.push(item);
+        } else if (style.flexWrap === 'nowrap' && isAutoMainSize) {
+            mainSpace -= itemStyle[mainSize];
+            if (itemStyle[crossSize] !== null && itemStyle[crossSize] !== (void 0)) {
+                // 行最高：交叉轴尺寸
+                crossSpace = Math.max(crossSpace, itemStyle[crossSize]);
+            } 
+            flexLine.push(item);
+        } else {
+            // 当前元素长度大于主轴
+            if (itemStyle[mainSize] > style[mainSize]) {
+                itemStyle[mainSize] = style[mainSize];
+            }
+
+            // 剩下空间不足以容纳当前元素
+            if (mainSpace < itemStyle[mainSize]) {
+                flexLine.mainSpace = mainSpace;
+                flexLine.crossSpace = crossSpace;
+                // 创建新行
+                flexLine = [item];
+                flexLines.push(flexLine);
+                mainSpace = style[mainSize];
+                crossSpace = 0;
+            } else {
+                flexLine.push(item);
+            }
+
+            if (itemStyle[crossSpace] !== null && itemStyle[crossSize] !== (void 0)) {
+                // 行最高：交叉轴尺寸
+                crossSpace = Math.max(crossSpace, itemStyle[crossSize]);
+            }
+
+            mainSpace -= itemStyle[mainSize];
+        }
+        flexLine.mainSpace = mainSpace;
+
+        console.log(items);
+    }
+
 }
 
 module.exprots = layout;
