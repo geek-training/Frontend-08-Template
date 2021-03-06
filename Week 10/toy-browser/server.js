912e8625e23c597c6f6f7f98469da8b84326b010
Liao Xueqing <xueqingliao@foxmail.com>
2021-03-06 13:44:31 +0800

    修改server生成图片

--- Week 10/toy-browser/server.js
+++ Week 10/toy-browser/server.js
@@ -7,32 +7,34 @@ const server = http.createServer((request, response) => {
     response.setHeader('X-Foo', 'bar');
     response.writeHead(200, {'Content-Type': 'text/plain'});
     response.end(
-`
-<html maa=a>
-    <head>
-        <style>
-            #container {
-                width: 500px;
-                height: 500px;
-                display: flex;
-            }
-            #container #myId {
-                width: 200px;
-            }
-            #container .c1 {
-                flex: 1;
-            }
-        </style>
-    </head>
-    <body>
-        <div id=container>
-            <div id="myId" />
-            <div class="ic1" />
-        </div>
-    </body>
+`<html lang="en">
+<head>
+  <style>
+    #container {
+      width:500px;
+      height:300px;
+      display:flex;
+      background-color: rgb(255,255,255);
+    }
+    #container #myid{
+      width:200px;
+      height:100px;
+      background-color: rgb(255,0,0);
+    }
+    #container .c1 {
+      flex:1;
+      background-color: rgb(0,255,0);
+    }
+  </style>
+</head>
+<body>
+      <div id="container">
+        <div id="myid"></div>
+        <div class="c1"></div>
+      </div>
+</body>
 </html>
-`
-    );
+`);
 });
 
 server.listen(8088);
