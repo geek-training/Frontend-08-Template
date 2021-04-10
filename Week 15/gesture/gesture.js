8b941ff205855abe0fc832b9d96321b28a9570b6
Liao Xueqing <xueqingliao@foxmail.com>
2021-04-11 01:35:39 +0800

    实现一个flick事件

--- Week 15/gesture/gesture.js
+++ Week 15/gesture/gesture.js
@@ -92,6 +92,11 @@ let start = (point, context) => {
 
     context.startX = point.clientX, context.startY = point.clientY;
 
+    context.points = [{
+        t: Date.now(),
+        x: point.clientX,
+        y: point.clientY,
+    }];
     context.isTap = true;
     context.isPan = false;
     context.isPress = false;
@@ -122,6 +127,13 @@ let move = (point, context) => {
         console.log("pan");
     }
 
+    context.points = context.points.filter(point => Date.now() - point.t < 500 );
+    context.points.push[{
+        t: Date.now(),
+        x: point.clientX,
+        y: point.clientY,
+    }]
+
 }
 
 let end = (point, context) => {
@@ -137,6 +149,24 @@ let end = (point, context) => {
         console.log("pressend");
     }
 
+    context.points = context.points.filter(point => Date.now() - point.t < 500 );
+    let v;
+    if (!context.points.length) {
+        v = 0;
+    } else {
+        let dx = point.clientX - context.points[0].x;
+        let dy = point.clientY - context.points[0].y;
+        let d = Math.sqrt(dx ** 2 + dy ** 2);
+        v = d / (Date.now() - context.points[0].t);
+    }
+
+    if (v > 1.5 ) {
+        console.log("flick");
+        context.isFlick = true;
+    } else {
+        context.isFlick = false;
+    }
+    console.log("v", v);
     // console.log('end', point.clientX, point.clientY); 
 }
 
