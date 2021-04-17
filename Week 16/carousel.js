556ed0bd993d5ffe830b4286de7eddc453a49ac2
Liao Xueqing <xueqingliao@foxmail.com>
2021-04-17 21:23:50 +0800

    添加pan和panend事件及timeline

--- Week 16/carousel.js
+++ Week 16/carousel.js
@@ -1,3 +1,5 @@
+import { Timeline } from '../Week 15/animation.js';
+import { ease } from '../Week 15/ease.js';
 import { Component } from './framework.js';
 import {enableGesture} from "./gesture/gesture";
 
@@ -24,6 +26,9 @@ export  class Carousel extends Component {
 
         enableGesture(this.root);
 
+        let timeline = new Timeline;
+        timeline.start();
+
         let children = this.root.children;
         let position = 0;
 
@@ -35,12 +40,44 @@ export  class Carousel extends Component {
 
             for (let offset of [-1, 0, 1]) {
                 let pos = current + offset;
-                pos = (pos + children.length) % children.length;
+                pos = (pos % children.length + children.length) % children.length;
                 children[pos].style.transition = "none";
                 children[pos].style.transform = `translateX(${- pos * 500 + offset * 500 + x % 500}px)`;
             }
 
         });
+
+        this.root.addEventListener('panend', event => {
+            let x = event.clientX - event.startX;
+                position = position - Math.round(x / 500);
+                for (let offset of [0, - Math.sign(Math.round(x / 500) - x + 250 * Math.sign(x))]) {
+                    let pos = position + offset;
+                    pos = (pos + children.length) % children.length;
+                    children[pos].style.transition = "";
+                    children[pos].style.transform = `translateX(${- pos * 500 + offset * 500}px)`;
+                }
+        });
+
+         setInterval(() => {
+            let children = this.root.children;
+
+            let nextIndex = (position + 1) % children.length;
+
+            let current = children[position];
+            let next = children[nextIndex];
+
+            next.style.transition = "none";
+            next.style.transform = `translateX(${500 - nextIndex * 500}px)`
+
+            // 创建animation
+            timeline.add(new Animation(current.style, "transform",
+             - position * 500, -500 - position * 500, 500, 0, ease, v => `translateX(${v}px)`))
+            timeline.add(new Animation(next.style, "transform",
+            500 - nextIndex * 500, - nextIndex * 500, 500, 0, ease, v => `translateX(${v}px)`));
+
+            position = nextIndex;
+        }, 3000);
+
 
         /**  鼠标时间轮播  */
 
