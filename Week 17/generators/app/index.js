724b51c9e8796f4236b7421d9e6b56f6b0751124
Liao Xueqing <xueqingliao@foxmail.com>
2021-05-01 23:33:29 +0800

    提供交互功能

--- Week 17/generators/app/index.js
+++ Week 17/generators/app/index.js
@@ -7,11 +7,22 @@ module.exports = class extends Generator {
         super(args, opts);
     }
 
-    method1() {
-        this.log('method 1 just ran');
-    }
-
-    method2() {
-        this.log('method 2 just ran');
+    async method1() {
+        const answers = await this.prompt([
+            {
+            type: "input",
+            name: "name",
+            message: "Your project name",
+            default: this.appname // Default to current folder name
+            },
+            {
+            type: "confirm",
+            name: "cool",
+            message: "Would you like to enable the Cool feature?"
+            }
+        ]);
+    
+        this.log("app name", answers.name);
+        this.log("cool feature", answers.cool);
     }
 };
